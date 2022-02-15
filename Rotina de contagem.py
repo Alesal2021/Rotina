1f8a8e5471b1ec5fa2e668d0150605a15762af7c
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-15 12:57:24 -0300

    Initial Commit

--- Rotina de contagem.py
+++ Rotina de contagem.py
@@ -1,7 +1,7 @@
 inicial = 0
 cont = 0
 while (inicial <= 5 ):
-    print(inicial,'', end= '')
+    print(inicial,'', end= '') #Caso queria um em baixo do outro so retirar o END '
     inicial += 1
 #print('') Caso precise pular uma linha
 ini = 0
